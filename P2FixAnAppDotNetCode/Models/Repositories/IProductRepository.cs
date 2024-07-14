e1774e50509b149679605ac1bd0e247c91b16009
Arthur <arthur.tripard@wearemip.com>
2024-07-14 17:31:51 +0200

    refactor: replace Product[] with List<Product> throughout the app

--- P2FixAnAppDotNetCode/Models/Repositories/IProductRepository.cs
+++ P2FixAnAppDotNetCode/Models/Repositories/IProductRepository.cs
@@ -1,8 +1,10 @@
-﻿namespace P2FixAnAppDotNetCode.Models.Repositories
+﻿using System.Collections.Generic;
+
+namespace P2FixAnAppDotNetCode.Models.Repositories
 {
     public interface IProductRepository
     {
-        Product[] GetAllProducts();
+        List<Product> GetAllProducts();
 
         Product GetProductById(int id);
 
--- P2FixAnAppDotNetCode/Models/Services/ProductService.cs
+++ P2FixAnAppDotNetCode/Models/Services/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using P2FixAnAppDotNetCode.Models.Repositories;
+using System.Collections.Generic;
 
 namespace P2FixAnAppDotNetCode.Models.Services
 {
@@ -19,10 +20,8 @@ namespace P2FixAnAppDotNetCode.Models.Services
         /// <summary>
         /// Get all product from the inventory
         /// </summary>
-        public Product[] GetAllProducts()
+        public List<Product> GetAllProducts()
         {
-            // TODO change the return type from array to List<T> and propagate the change
-            // throughout the application
             return _productRepository.GetAllProducts();
         }
 
